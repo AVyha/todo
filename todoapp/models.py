39d45b7716e8b9629edd05d4ed012d37cb177e22
Artem <Artemvyhovanec@gmail.com>
2022-11-14 12:48:00 +0200

    create models

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    content = models.CharField(max_length=255)
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(blank=True, null=True)
+    completed = models.BooleanField(default=False)
+    tags = models.ForeignKey("Tag", on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ["completed", "-datetime"]
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
