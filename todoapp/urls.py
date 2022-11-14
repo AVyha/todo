0cb7e976c159289303678fc751df69e625551f30
Artem <Artemvyhovanec@gmail.com>
2022-11-14 13:28:12 +0200

    add CRUD for tag and crispy forms

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -4,7 +4,10 @@ from todoapp.views import *
 
 urlpatterns = [
     path("", index, name="index"),
-    path("tag/", TagListView.as_view(), name="todo-list")
+    path("tag/", TagListView.as_view(), name="tag-list"),
+    path("tag/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tag/update/<int:pk>/", TagUpdateView.as_view(), name="tag-update"),
+    path("tag/delete/<int:pk>/", TagDeleteView.as_view(), name="tag-delete"),
 ]
 
 app_name = "todo"
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
 from todoapp.models import Task, Tag
@@ -10,3 +11,20 @@ def index(request):
 
 class TagListView(generic.ListView):
     model = Tag
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    success_url = reverse_lazy("todo:tag-list")
+    fields = "__all__"
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    success_url = reverse_lazy("todo:tag-list")
+    fields = "__all__"
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("todo:tag-list")
